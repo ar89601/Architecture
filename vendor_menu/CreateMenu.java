5da36bade2e0a214ad50e99e24a3b3c57d5e6c43
ar89601 <ar89601@gmail.com>
2017-06-12 16:03:57 -0400

    Create CreateMenu.java

---
+++ vendor_menu/CreateMenu.java
@@ -0,0 +1,13 @@
+/**
+ * @author Ashley.Ridout
+ * @date June 12, 2017
+ *
+ */
+package vendor_menu;
+
+//re-usable interface to create menus 
+public interface CreateMenu {
+	public void addItem();
+	public void addCondiment();
+	public void addSize();
+}
