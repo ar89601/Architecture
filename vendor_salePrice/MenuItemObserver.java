d5f9d8828c0eb6edfb5aeeedb0b0a3c2dd2f9a6c
ar89601 <ar89601@gmail.com>
2017-06-12 16:09:17 -0400

    Create MenuItemObserver.java

---
+++ vendor_salePrice/MenuItemObserver.java
@@ -0,0 +1,12 @@
+/**
+ * @author Ashley.Ridout
+ * @date June 11, 2017
+ *
+ */
+package vendor_salePrice;
+
+// abstract parent class for all observers
+public abstract class MenuItemObserver {
+		protected Source source;
+		public abstract void update();
+}
